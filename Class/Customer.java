2f5993a633d6a2bdebb6cd576e38115a38b0efe0
Brent Robinson <brent_robinson@hotmail.com>
2017-10-27 14:50:41 -0400

    added the customer class
    matches customer table in database

---
+++ Class/Customer.java
@@ -0,0 +1,51 @@
+package com.company;
+
+public class Customer {
+
+    private String fName;
+    private String lName;
+    private String paymentType;
+    private String phoneNumber;
+    private String address;
+
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getPaymentType() {
+        return paymentType;
+    }
+
+    public void setPaymentType(String paymentType) {
+        this.paymentType = paymentType;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
